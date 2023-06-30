aa5df2c040f2ec3660e8f1e0f3e85f134277e292
Marija Kocevska <kocevska_m@hotmail.com>
2023-06-30 09:59:13 +0100

    task 13 to main

--- api/listen.js
+++ api/listen.js
@@ -1,8 +1,8 @@
 
 const app = require(`${__dirname}/app`)
 
-const {PORT} = process.env;
+const {PORT = 9090} = process.env;
 
 app.listen(PORT, () => {
     console.log("Server is listening on port 9090")
-})
+});
